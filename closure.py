d9fe8cdb6d55076561972a5d9cb9f1a20dceacb2
qmfw <90566797+qmfw@users.noreply.github.com>
2022-11-18 17:35:12 +0900

    Create closure.py

---
+++ closure.py
@@ -0,0 +1,52 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    printer()
+
+# We execute the function
+# Output: Hello
+print_msg("Hello")
+
+
+
+
+def psg(msg):
+    # This is the outer enclosing function
+
+    def pnt():
+        # This is the nested function
+        print(msg)
+
+    return pnt  # returns the nested function
+
+# Now let's try calling this function.
+# Output: Hello
+another = psg("Hello2")
+another()
+
+
+
+
+def make_multiplier_of(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+# Multiplier of 3
+times3 = make_multiplier_of(3)
+
+# Multiplier of 5
+times5 = make_multiplier_of(5)
+
+# Output: 27
+print(times3(9))
+
+# Output: 15
+print(times5(3))
+
+# Output: 30
+print(times5(times3(2)))
